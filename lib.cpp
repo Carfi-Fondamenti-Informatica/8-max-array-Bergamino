4fd7fe5ccb9534b4c57521813901895dd072a909
Bergamino <91837242+Bergamino@users.noreply.github.com>
2021-12-07 18:19:18 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-int funzione (int a){
-    int d=0,g=0,r=0,numeri[a];
-    for (int i=0;i<a;i++){
-        cin >> numeri[r];
-        d=numeri[r];
-        r++;
-        if(d>g){
-            g=d;
-        }else if((i==0)&&(d<0)){
-            g=d;
+
+int somma(int numeri[], int dimensione){
+    int sum = 0,q=0;
+    for(int i=0;i < dimensione; i++){
+        cin>>numeri[i];
+        if (numeri[i]>q){
+            q=numeri[i];
         }
+        sum += numeri[i];
     }
-    return g;
+    return q;
 }
