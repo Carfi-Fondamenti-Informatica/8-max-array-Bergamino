4f58c646af5a9c4367e55f2e4c5f6fa7fb5cefe7
Bergamino <91837242+Bergamino@users.noreply.github.com>
2021-12-07 18:17:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "lib.h"
+
 using namespace std;
 
 int main() {
     int a=0;
     cin >> a;
-    cout << funzione (a) <<endl;
+    int numeri[a];
+    cout << somma(numeri, a)<<endl;
     return 0;
 }
