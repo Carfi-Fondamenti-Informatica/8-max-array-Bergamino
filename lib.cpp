701c94a503f1bc95814f1461213dedf2dba60510
Bergamino <91837242+Bergamino@users.noreply.github.com>
2021-12-07 14:54:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+#include "lib.h"
+using namespace std;
+int funzione (int a){
+    int d=0,g=0,r=0,numeri[a];
+    for (int i=0;i<a;i++){
+        cin >> d;
+        numeri[r]=d;
+        ++r;
+        if(d>g){
+            g=d;
+        }
+    }
+    return g;
+}
