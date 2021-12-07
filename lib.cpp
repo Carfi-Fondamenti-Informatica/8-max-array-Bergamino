13d2ed14bd798ea02bb4c1af0203e479bd91e031
Bergamino <91837242+Bergamino@users.noreply.github.com>
2021-12-07 17:39:46 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int funzione (int a){
     int d=0,g=0,r=0,numeri[a];
     for (int i=0;i<a;i++){
-        cin >> d;
-        numeri[r]=d;
-        ++r;
+        cin >> numeri[r];
+        d=numeri[r];
+        r++;
         if(d>g){
+            g=d;
+        }else if((i==0)&&(d<0)){
             g=d;
         }
     }
     return g;
-} 
+}
