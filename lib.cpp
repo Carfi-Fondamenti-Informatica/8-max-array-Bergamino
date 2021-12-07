f2ea6e053115d83c94fe1c95164c76a81148e9ae
Bergamino <91837242+Bergamino@users.noreply.github.com>
2021-12-07 18:32:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 
 int somma(int numeri[], int dimensione){
-    int sum = 0,q=0;
+    int q=0;
     for(int i=0;i < dimensione; i++){
         cin>>numeri[i];
         if (numeri[i]>q){
             q=numeri[i];
         }
-        sum += numeri[i];
     }
     return q;
 }
