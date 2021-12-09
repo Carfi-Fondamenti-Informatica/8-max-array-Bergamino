eb6508410627fc31573a67b1f302ef1c3c6e896d
Bergamino <91837242+Bergamino@users.noreply.github.com>
2021-12-09 11:08:40 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,12 +2,14 @@
 #include "lib.h"
 using namespace std;
 
+
 float funzione (int a){
-    int g=0,r=0;
+    int r=0;
+    float g=0, d=0;
     float numeri[a];
     for (int i=0;i<a;i++){
         cin >> numeri[r];
-        float d = numeri[r];
+        d = numeri[r];
         r++;
         if(d>g){
             g=d;
